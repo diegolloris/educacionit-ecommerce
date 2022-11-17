e43c7ce25b393788c3e628c5267d4a69fa6d76e8
Ezequiel Salomon <ezequielsalomon@BA-MAC-EzequielS.fibertel.com.ar>
2022-11-16 22:14:49 -0300

    agregado indexMioMio estatico y modificado index

---
+++ src/indexMioMio.js
@@ -0,0 +1,21 @@
+export function indexMioMio(params) {
+    return(
+        <>
+        <div className="topBody" >
+            <h2 className="title">MioMio</h2>
+            <button className="btn-carrito">Carrito</button>
+        </div>
+
+        <div className="body">
+            <span className="search-bar" inputMode="text" placeholder="Tu busqueda"></span>
+
+            <div className="product">
+                <img src=""></img>
+                <></>
+
+            </div>
+        </div>    
+        
+        </>
+    )
+}
