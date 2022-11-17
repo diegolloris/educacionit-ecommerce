e33e35f473448fe2cf7e900b02a36a2b0527d611
Diego <diego.lloris@gmail.com>
2022-11-17 20:24:28 -0300

    cambios de estilo

---
+++ src/IndexMioMio.js
@@ -0,0 +1,33 @@
+export function IndexMioMio(params) {
+    return(
+        <>
+        <div className="carrito" >
+            <h2>MioMio</h2>
+            <div className="center">
+                <button>Carrito</button>
+            </div> 
+            <hr />
+        </div>
+
+        <div className="body">
+            <div className="center">
+                <input type="text" placeholder="Tu busqueda"></input>
+            </div>
+            <br />
+            <div className="center">
+                <img src="/images/Anteojos/ali-pazani-GwglcplmXDs-unsplash.jpg.webp" className="thumbs" />
+            </div>
+            <br />
+            <div className="center">
+                <img src="/images/Anteojos/Anteojos.jpg.webp" className="thumbs" />
+            </div>
+            <br />
+            <div className="center">
+                <img src="/images/Anteojos/apostolos-vamvouras-mKi4QEJXRCs-unsplash.jpg.webp" className="thumbs" />
+            </div>
+            <br />
+        </div>    
+        
+        </>
+    )
+}
